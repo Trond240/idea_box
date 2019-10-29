731b955e5d9453fc3fa461a000106461cc85cec1
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2019-10-29 17:00:50 -0600

    Remove comments

--- idea.js
+++ idea.js
@@ -17,17 +17,12 @@ class Idea {
 
   deleteFromStorage(event) {
     var fromStorage = retrieveIdeas();
-// Creating new array to be set in localStorage
     var newIdeaArray = [];
     for (var i = 0; i < fromStorage.length; i++) {
       if (this.id !== fromStorage[i].id) {
-// Checking for the id that matches and creating an array of everything that does not
         newIdeaArray.push(fromStorage[i]);
       }
     }
-// Sending the new array to localStorage as a string
     localStorage.setItem("ideaLocalStorage", JSON.stringify(newIdeaArray));
   }
 }
-
-// module.exports = Idea;
