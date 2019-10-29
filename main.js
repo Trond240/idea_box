982bfade3406ea7646afc766f5b6ba48e53da5c8
Grayson Palmer <gpalmer1385@gmail.com>
2019-10-29 16:34:44 -0600

    Clean up of console logs from testing

--- main.js
+++ main.js
@@ -113,6 +113,5 @@ function asideHandler(event) {
   if (event.target.classList.contains("mobile-menu-inactive")) {
     event.target.classList.toggle("mobile-menu-active");
     event.path[1].children[2].classList.toggle("starred-ideas-active");
-    console.log(event);
   }
 }
